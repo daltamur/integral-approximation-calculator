76173c9ee9c6416e2eb6413a747fdcb0585d1352
Dominic Altamura <daltamur@oswego.edu>
2020-11-15 20:53:21 -0500

    Delete Operator.java

--- Operator.java
+++
@@ -1,7 +0,0 @@
-public enum Operator {
-   LPARENTHESIS(0),RPARENTHESIS(1),PLUS(2),MINUS(3),MULTIPLY(4),DIVIDE(5),TIMES(6);
-   final int precedence;
-    Operator(int precedence){
-      this.precedence=precedence;
-   }
-}
